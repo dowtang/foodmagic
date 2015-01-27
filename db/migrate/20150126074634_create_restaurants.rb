81c5a42e40fce2ec93efdd7c1c2849a3827302fa
Dow  Tang <dowtang1@Dows-MacBook-Air.local>
2015-01-27 15:33:58 +0800

    restaurant scrape database done

---
+++ db/migrate/20150126074634_create_restaurants.rb
@@ -0,0 +1,16 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :cuisinetype
+      t.string :location
+      t.string :logourl
+      t.string :deliverytime
+      t.string :deliverycost
+      t.string :minimumorder
+      t.string :paymenttype
+      t.string :reviewcount
+      t.timestamps
+    end
+  end
+end
