5cfaa9765e512b01816a957feedd62f1749cac62
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-01-28 10:38:10 +0800

    deleted the new create param in restaurants_controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,31 +8,4 @@ class RestaurantsController < ApplicationController
     @reataurant = Restaurant.find(params[:id])
   end
 
-  def new
-    @restaurant = Restaurant.new
-  end
-
-  # def create
-  #   @restaurant = Restaurant.new(:code_id => params[:code_id], :restaurant_name => params[:restaurant_name], :restaurant_add => params[:restaurant_add], :restaurant_logo => params[:restaurant_logo], :restaurant_district => params[:restaurant_district], :delivery_time => params[:delivery_time], :free_delivery => params[:free_delivery], :min_delivery => params[:min_delivery], :payment_method => params[:payment_method])
-    # @restaurant = Restaurant.new(product_params)
-
-  #     if @restaurant.save
-  #       redirect_to products_path
-  #       else
-  #       redirect_to :back
-  #     end
-  # end
-
-  def edit
-
-  end
-
-  private
-    def product_params
-      params.require(:restaurant).
-        permit(:code_id, :restaurant_name, :restaurant_add, :restaurant_logo, :restaurant_district, :delivery_time, :free_delivery, :min_delivery, :payment_method)
-    end
-
-
-
 end
