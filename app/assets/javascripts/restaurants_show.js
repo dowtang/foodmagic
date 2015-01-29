29a033d2d8b768802a296ac52a64625a4e0a42d1
TamVivien <VivienTam@TamViviens-MacBook-Pro.local>
2015-01-29 14:53:15 +0800

    got the tag menu/reviews/info on menu page work

---
+++ app/assets/javascripts/restaurants_show.js
@@ -0,0 +1,31 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready(function(){
+
+  $("#menu-button-menu").click(function(){
+    
+    $("#tab-reviews").hide();
+    $("#tab-info").hide();
+    $("#tab-menu").show();
+  });
+
+
+  $("#menu-button-reviews").click(function(){
+    
+    $("#tab-menu").hide();
+    $("#tab-info").hide();
+    $("#tab-reviews").show();
+  });
+
+  $("#menu-button-info").click(function(){
+    
+    $("#tab-menu").hide();
+    $("#tab-reviews").hide();
+    $("#tab-info").show();
+  });
+
+
+
+});
+
